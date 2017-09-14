cb72ab394180bb74d0bf72577265db158d13f5dd
JonnyzGaming <31124239+jonnyzGaming@users.noreply.github.com>
2017-09-14 23:02:56 +0100

    added eclipse testapp workspace to store files

---
+++ basic.py
@@ -0,0 +1,18 @@
+import random
+import sys
+import os
+
+#comment
+"""
+Multi line comment with 3 quotes
+"""
+
+#5 main types in python: Numbers, Strings, Lists, Tuples, Dictonaries
+#operaters +,-,*,/,%,//,**
+
+name = '''hello mate
+what up
+bob'''
+
+print("%s", name)
+
